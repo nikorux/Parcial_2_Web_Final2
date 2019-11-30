c87f7979ba7fdb62fbb015f62691b93c2200dac3
Nikorux <nicole.0126@hotmail.es>
2019-11-30 14:34:27 -0400

    Manejo de usuarios

--- src/main/java/edu/pucmm/parcial2/Encapsulation/User.java
+++ src/main/java/edu/pucmm/parcial2/Encapsulation/User.java
@@ -5,5 +5,35 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
-public class User {
+@Entity
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String name;
+    private String password;
+    private boolean admin;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "user")
+    private Set<Url> urls;
+
+    public User() {
+    }
+
+    public User(String username, String name, String password, boolean admin, Set<Url> urls) {
+        this.username = username;
+        this.name = name;
+        this.password = password;
+        this.admin = admin;
+        this.urls = urls;
+    }
+
+    public User(String username, String name, String password, boolean admin) {
+        this.username = username;
+        this.name = name;
+        this.password = password;
+        this.admin = admin;
+        this.urls = new HashSet<>();
+    }
 }
